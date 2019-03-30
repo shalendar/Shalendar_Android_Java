6d73be6570e1c4be7f72ab2b2fb5692932d073d9
JINKOO <jacob456@hanmail.net>
2019-03-30 14:20:39 +0900

    Login to Main

---
+++ app/src/main/java/kr/ac/smu/cs/shalendar_java/CodeNumber.java
@@ -0,0 +1,8 @@
+package kr.ac.smu.cs.shalendar_java;
+
+public class CodeNumber {
+
+    public static int TO_MAIN_ACTIVITY = 1000;
+    public static int TO_LOGIN_ACTIVITY = 1001;
+    public static int TO_CREATE_MEMBER_ACTIVITY = 1002;
+}
---
+++ app/src/main/java/kr/ac/smu/cs/shalendar_java/LoginActivity.java
@@ -0,0 +1,41 @@
+package kr.ac.smu.cs.shalendar_java;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+/*
+    Login하는 Activity
+    Login -> MainActivity로 넘어간다.
+
+ */
+
+public class LoginActivity extends AppCompatActivity {
+
+    //button 멤버 변수
+    private Button buttonToMain;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        buttonToMain = findViewById(R.id.login_toMain_button);
+
+
+        /*
+           우선 버튼 클릭시 MainActivity로 넘어간다.
+           - 나중에 CreateMemberActivity로 넘어가는 코드 짜야 한다.
+         */
+        buttonToMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivityForResult(intent, CodeNumber.TO_MAIN_ACTIVITY);
+
+            }
+        });
+    }
+}
