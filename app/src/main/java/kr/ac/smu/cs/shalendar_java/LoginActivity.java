c9703ea2d4eeea1f2116f5aba92f0a8d1c06f4db
Parksungjoon <Lion.novjoon@gmail.com>
2019-05-01 06:22:07 +0900

    게시판 화면

--- app/src/main/java/kr/ac/smu/cs/shalendar_java/LoginActivity.java
+++ app/src/main/java/kr/ac/smu/cs/shalendar_java/LoginActivity.java
@@ -22,6 +22,7 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+
         buttonToMain = findViewById(R.id.login_toMain_button);
 
 
