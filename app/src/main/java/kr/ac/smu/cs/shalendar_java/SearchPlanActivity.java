55ed75486792ee6fc90bc4f6ef90503136b827cd
Parksungjoon <Lion.novjoon@gmail.com>
2019-05-22 15:05:39 +0900

    검색화면

---
+++ app/src/main/java/kr/ac/smu/cs/shalendar_java/SearchPlanActivity.java
@@ -0,0 +1,21 @@
+package kr.ac.smu.cs.shalendar_java;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class SearchPlanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_plan);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+}
