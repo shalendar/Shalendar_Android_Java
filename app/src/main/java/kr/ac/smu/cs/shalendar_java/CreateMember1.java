cf788faa66b3e3ac8e775c92f3079886bf68a57b
JINKOO <jacob456@hanmail.net>
2019-07-20 02:18:53 +0900

    회원가입 통신 성공, 로그인 통신코드입힘

--- app/src/main/java/kr/ac/smu/cs/shalendar_java/CreateMember1.java
+++ app/src/main/java/kr/ac/smu/cs/shalendar_java/CreateMember1.java
@@ -3,30 +3,47 @@ package kr.ac.smu.cs.shalendar_java;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class CreateMember1 extends AppCompatActivity {
 
+    private EditText editTextUserEmail;
     private Button buttonToMember2;
+    private String userEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_member1);
 
+        editTextUserEmail = (EditText)findViewById(R.id.create_member1_editText1);
         buttonToMember2 = findViewById(R.id.create_member2_button);
 
-        buttonToMember2.setOnClickListener(new View.OnClickListener() {
 
+        buttonToMember2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent2 = new Intent(getApplicationContext(), CreateMember2.class);
-                startActivityForResult(intent2, CodeNumber.TO_CREATE_MEMBER2);
+                //회원 가입 2번째로 보낼 userEmail
+                userEmail = editTextUserEmail.getText().toString().trim();
+                Log.d("Email", userEmail);
 
+                if(userEmail.equals("")) {
+                    Toast.makeText(getApplicationContext(), "Email을 입력하세요~", Toast.LENGTH_LONG).show();
+                }
+                else if(!userEmail.contains("@")) {
+                    Toast.makeText(getApplicationContext(), "잘못된 Email형식입니다", Toast.LENGTH_LONG).show();
+                }
+                else{
+                    Intent intent = new Intent(getApplicationContext(), CreateMember2.class);
+                    intent.putExtra("userEmail", userEmail);
+                    startActivityForResult(intent, CodeNumber.TO_CREATE_MEMBER2);
+                }
             }
         });
-
-
     }
 }
---
+++ app/src/main/java/kr/ac/smu/cs/shalendar_java/NetWorkUrl.java
@@ -0,0 +1,19 @@
+package kr.ac.smu.cs.shalendar_java;
+/*
+  서버 URL이 매일 바뀌니
+  여기서 toServerUrl 멤버변수만 바꾸면 됨
+  각 Activity에서 통신 할 때는 getServerUrl()호출하여 해당 경로 더 붙여줘야
+
+  ex)
+  CreateMember(회원 가입) Activity에서는
+  통신 URL :: getServerUrl() + /signup
+ */
+public class NetWorkUrl {
+
+    private String toServer_URL = "https://b8b3c17a.ngrok.io/MIND";
+
+    public String getServerUrl() {
+        return this.toServer_URL;
+    }
+
+}
