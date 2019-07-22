ac4aa18f5645a197f5af8ff5f8396ccfd1b9ccbb
Parksungjoon <Lion.novjoon@gmail.com>
2019-07-22 20:05:50 +0900

    사이드바 캘린더부분 구현

---
+++ app/src/main/java/kr/ac/smu/cs/shalendar_java/SidebarTeamItem.java
@@ -0,0 +1,19 @@
+package kr.ac.smu.cs.shalendar_java;
+
+import android.widget.ImageView;
+
+public class SidebarTeamItem {
+    int teammatePic;
+
+    public SidebarTeamItem(int teammatePic) {
+        this.teammatePic = teammatePic;
+    }
+
+    public int getTeammatePic() {
+        return teammatePic;
+    }
+
+    public void setTeammatePic(int teammatePic) {
+        this.teammatePic = teammatePic;
+    }
+}
