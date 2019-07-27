2e9884c52de51de8d587b934c08548140a82211c
JINKOO <jacob456@hanmail.net>
2019-07-27 18:28:58 +0900

    일정 상세보기 통신 완료

--- app/src/main/java/kr/ac/smu/cs/shalendar_java/BoarderAdapter.java
+++ app/src/main/java/kr/ac/smu/cs/shalendar_java/BoarderAdapter.java
@@ -3,6 +3,7 @@ package kr.ac.smu.cs.shalendar_java;
 
 import android.app.Activity;
 import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -10,6 +11,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,6 +20,10 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.gson.JsonObject;
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.Ion;
 
 import java.util.ArrayList;
 
@@ -124,6 +130,9 @@ public class BoarderAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder
 
     class ItemViewHolder extends RecyclerView.ViewHolder {
 
+        //서버 통신
+        private NetWorkUrl url = new NetWorkUrl();
+
         private TextView planname_text;
         private TextView plandate_text;
         private TextView planlocation_text;
@@ -139,14 +148,98 @@ public class BoarderAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder
             planlocation_text = itemView.findViewById(R.id.location);
             planreply_text = itemView.findViewById(R.id.replynum);
 
+
+            //통신 준비
+            Ion.getDefault(itemView.getContext()).configure().setLogging("ion-sample", Log.DEBUG);
+            Ion.getDefault(itemView.getContext()).getConscryptMiddleware().enable(false);
+
+            /*
+              해당 item클릭시 서버에서 정보 받고
+              이 정보들을 intent로 저장 시킨 후
+              PlanDetail로 남긴다.
+             */
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     Toast.makeText(v.getContext(), boardList.get(getAdapterPosition()-1).getPlanname(),Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(context, PlanDetailActivity.class);
-                    //어댑터에서는 이렇게 해줘야함
-                    //해당 plandetail로 이동
-                    context.startActivity(intent);
+
+
+                    JsonObject json = new JsonObject();
+
+                    json.addProperty("sid", 10);
+
+                    final ProgressDialog progressDialog = new ProgressDialog(itemView.getContext());
+                    progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+                    progressDialog.setMessage("잠시만 기다려주세요. 해당 일정 등록 중 입니다~");
+                    progressDialog.show();
+
+
+                    Ion.with(itemView.getContext())
+                            .load("POST", url.getServerUrl() + "/showSche")
+                            .setHeader("Content-Type", "application/json")
+                            .setJsonObjectBody(json)
+                            .asJsonObject()
+                            .setCallback(new FutureCallback<JsonObject>() {
+                                @Override
+                                public void onCompleted(Exception e, JsonObject result) {
+
+                                    String userName, schedTitle, aboutSched, schedLocation;
+                                    String startDate, startTime, endDate, endTime, startToEnd;
+
+                                    if(e != null) {
+                                        Toast.makeText(itemView.getContext(), "Server Connection Error!", Toast.LENGTH_LONG).show();
+                                    }
+
+                                    else {
+                                        //응답 형식이 { "data":{"id":"jacob456@hanmail.net", "cid":1, "sid":10, "title":"korea"}, "message":"success"}
+                                        //data: 다음에 나오는 것들도 JsonObject형식.
+                                        //따라서 data를 JsonObject로 받고, 다시 이 data를 이용하여(어찌보면 JsonObject안에 또다른 JsonObject가 있는 것이다.
+                                        //JSONArray가 아님. 얘는 [,]로 묶여 있어야 함.
+
+                                        String message = result.get("message").getAsString();
+                                        //서버로 부터 응답 메세지가 success이면...
+
+                                        if(message.equals("success")) {
+                                            //서버 응답 오면 로딩 창 해제
+                                            progressDialog.dismiss();
+
+                                            //data: {} 에서 {}안에 있는 것들도 JsonObject
+                                            JsonObject data = result.get("data").getAsJsonObject();
+
+                                            userName = data.get("id").getAsString();
+                                            schedTitle = data.get("title").getAsString();
+                                            aboutSched = data.get("sContent").getAsString();
+                                            schedLocation = data.get("area").getAsString();
+                                            startDate = data.get("startDate").getAsString();
+                                            startTime = data.get("startTime").getAsString();
+                                            endDate = data.get("endDate").getAsString();
+                                            endTime = data.get("endTime").getAsString();
+
+                                            startToEnd = startDate + " " + startTime + " ~ " + endDate + " " + endTime;
+
+                                            Intent intent = new Intent(context, PlanDetailActivity.class);
+                                            intent.putExtra("userName", userName);
+                                            intent.putExtra("schedTitle", schedTitle);
+                                            intent.putExtra("aboutSched", aboutSched);
+                                            intent.putExtra("area", schedLocation);
+                                            intent.putExtra("startToEnd", startToEnd);
+
+                                            context.startActivity(intent);
+
+                                            Log.i("result",data.get("id").getAsString());
+                                        } else {
+
+                                            Toast.makeText(itemView.getContext(), "해당 일정이 없습니다.", Toast.LENGTH_LONG).show();
+                                        }
+
+                                    }
+                                }
+                            });
+
+//                    Intent intent = new Intent(context, PlanDetailActivity.class);
+//                    //어댑터에서는 이렇게 해줘야함
+//                    //해당 plandetail로 이동
+//                    context.startActivity(intent);
                 }
             });
 
