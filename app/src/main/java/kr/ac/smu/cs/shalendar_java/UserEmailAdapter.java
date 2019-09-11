ea1ef0f0ed955137d9626c89efa6ca8651f0d56b
JINKOO <jacob456@hanmail.net>
2019-09-11 17:33:53 +0900

    통신코드 수정

--- app/src/main/java/kr/ac/smu/cs/shalendar_java/UserEmailAdapter.java
+++ app/src/main/java/kr/ac/smu/cs/shalendar_java/UserEmailAdapter.java
@@ -96,20 +96,22 @@ public class UserEmailAdapter extends RecyclerView.Adapter<UserEmailAdapter.View
         }
 
         public void setItem(UserEmail item) {
-            userImage.setBackground(new ShapeDrawable(new OvalShape()));
-            if (Build.VERSION.SDK_INT >= 21) {
-                userImage.setClipToOutline(true);
-            }
-            if (item.getUserImageURL().equals("DEFAULT :: profile_IMAGE")) {
-                userImage.setImageResource(R.drawable.profile_default);
-            } else {
-                Ion.with(userImage)
-                        .centerCrop()
-                        .resize(80, 80)
-                        .load(item.getUserImageURL());
-            }
-            userImage.setBackground(new ShapeDrawable(new OvalShape()));
-            userImage.setClipToOutline(true);
+            Global global = new Global();
+            global.setProfileImage(userImage, item.getUserImageURL());
+//            userImage.setBackground(new ShapeDrawable(new OvalShape()));
+//            if (Build.VERSION.SDK_INT >= 21) {
+//                userImage.setClipToOutline(true);
+//            }
+//            if (item.getUserImageURL().equals("DEFAULT :: profile_IMAGE")) {
+//                userImage.setImageResource(R.drawable.profile_default);
+//            } else {
+//                Ion.with(userImage)
+//                        .centerCrop()
+//                        .resize(80, 80)
+//                        .load(item.getUserImageURL());
+//            }
+//            userImage.setBackground(new ShapeDrawable(new OvalShape()));
+//            userImage.setClipToOutline(true);
             userEmail.setText(item.getUserEmail());
 //            checkBox.setChecked(item.getIs_checked());
         }
